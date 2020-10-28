7d5f11e4691969d78af37fce8a1626d465fb230a
bdcoelho <ben_coelho@hotmail.com>
2020-10-28 11:28:24 +1100

    Updated resume and project links

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,59 +1,60 @@
-import farmersMarket from '../assets/img/farmers-market.png';
-import employeeDirectory from '../assets/img/employee-directory.png';
-import burgerLogger from '../assets/img/burger-logger.png';
-import weatherDashboard from '../assets/img/weather-dashboard.png';
-import workCalendar from '../assets/img/work-calendar.png';
-import noteTaker from '../assets/img/note-taker.png';
-import workoutTracker from '../assets/img/workout-tracker.png';
-import codeQuiz from '../assets/img/code-quiz.png';
+import farmersMarket from "../assets/img/farmers-market.png";
+import employeeDirectory from "../assets/img/employee-directory.png";
+import howdyNeighbour from "../assets/img/howdy-neighbour.png";
+import weatherDashboard from "../assets/img/weather-dashboard.png";
+import workCalendar from "../assets/img/work-calendar.png";
+import noteTaker from "../assets/img/note-taker.png";
+import workoutTracker from "../assets/img/workout-tracker.png";
+import codeQuiz from "../assets/img/code-quiz.png";
 
 export default [
+  {
+    name: "Howdy Neighbour!",
+    image: howdyNeighbour,
+    app: "https://mysterious-inlet-60468.herokuapp.com/",
+    github: "https://github.com/bdcoelho/Project3",
+  },
+
   {
     name: "Farmer's Market",
     image: farmersMarket,
     app: "https://p2idea.herokuapp.com/",
-    github: "https://github.com/lcerdeira/p2idea"
+    github: "https://github.com/lcerdeira/OrganicS2",
   },
   {
     name: "Employee Directory",
     image: employeeDirectory,
     app: "https://bdcoelho.github.io/employee-directory/",
-    github: "https://github.com/bdcoelho/employee-directory"
-  },
-  {
-    name: "Burger Logger",
-    image: burgerLogger,
-    app: "https://lit-chamber-58185.herokuapp.com/",
-    github: "https://github.com/bdcoelho/burger"
+    github: "https://github.com/bdcoelho/employee-directory",
   },
   {
     name: "Weather Dashboard",
     image: weatherDashboard,
     app: "https://bdcoelho.github.io/Weather-Dashboard/",
-    github: "https://github.com/bdcoelho/Weather-Dashboard"
+    github: "https://github.com/bdcoelho/Weather-Dashboard",
   },
   {
     name: "Work Calendar",
     image: workCalendar,
     app: "https://bdcoelho.github.io/Calendar/",
-    github: "https://github.com/bdcoelho/Calendar"
+    github: "https://github.com/bdcoelho/Calendar",
   },
   {
     name: "Code Quiz",
     image: codeQuiz,
     app: "https://bdcoelho.github.io/CodeQuiz/",
-    github: "https://github.com/bdcoelho/CodeQuiz"
+    github: "https://github.com/bdcoelho/CodeQuiz",
   },
   {
     name: "Note Taker",
     image: noteTaker,
     app: "https://desolate-meadow-17602.herokuapp.com/",
-    github: "https://github.com/bdcoelho/note-taker"
+    github: "https://github.com/bdcoelho/note-taker",
   },
   {
     name: "Workout Tracker",
     image: workoutTracker,
     app: "https://dry-lowlands-11752.herokuapp.com/",
-    github: "https://github.com/bdcoelho/Workout-Tracker"
-  }
-]
+    github: "https://github.com/bdcoelho/Workout-Tracker",
+  },
+];

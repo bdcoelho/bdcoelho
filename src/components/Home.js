7d6f9c0ca3c5bed4e41be302d5f45b26679d50dc
bdcoelho <ben_coelho@hotmail.com>
2021-01-24 11:18:03 +1100

    Before split to client/server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,54 @@
-import React from 'react';
-import './index.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import { animateScroll as scroll, scroller } from 'react-scroll';
+import React from "react";
+import "./index.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { animateScroll as scroll, scroller } from "react-scroll";
 
 function Home() {
   const handleButtonClicked = () => {
-    scroller.scrollTo('about', {
+    scroller.scrollTo("about", {
       smooth: true,
     });
-  }
+  };
 
   return (
     <section id="home" className="d-flex align-items-center">
       <Container>
+        <Row className="intro-banner">
+          <Col>
+          
         <Row>
           <Col className="d-flex justify-content-center col-md-12">
             <p className="intro">
-              Hi, I'm <span className="name">Benson Coelho</span>.
-              <br />
-              Engineer and Full-stack Developer
+              Monsoon Smokehouse
             </p>
+
           </Col>
+        </Row>
+        <Row>
+        <Col className="d-flex justify-content-center col-md-12">
+        <p className="sub-intro">
+Authentic Goan sausages from Melbourne
+            </p>
+</Col>
+            </Row>
+</Col>
+</Row>
+        <br></br>
+        <Row>
           <Col className="d-flex justify-content-center col-md-12">
             <Button
               type="button"
               className="btn btn-outline-primary d-flex align-items-center btn-home"
               onClick={handleButtonClicked}
             >
-              <div>See more</div>
+              <div>Find out more</div>
               <i className="fa fa-arrow-right"></i>
             </Button>
           </Col>
         </Row>
       </Container>
     </section>
-  )
+  );
 }
 
 export default Home;
